2f99dd8ad241df5dda294c18c144154f3dea1158
omanpeko <79346126+omanpeko@users.noreply.github.com>
2025-10-07 14:46:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ async def teamtest(ctx):
     embed = discord.Embed(title="チーム分け結果", color=main_color)
 
     embed.add_field(
-        name="🟥 アタッカー______",
+        name="🟥 アタッカー＿＿＿＿",
         value="\n".join([format_player_line(p) for p in teamA]) + f"\n戦力：{powerA}",
         inline=True
     )
